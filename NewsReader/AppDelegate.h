f568f0e37b3ec737770aea1a07fe4e3896cd36f2
Antonio Rodrigues <aaadonai@gmail.com>
2013-03-27 17:22:03 +1100

    Initial Commit

---
+++ NewsReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NewsReader
+//
+//  Created by Antonio Rodrigues on 27/03/13.
+//  Copyright (c) 2013 Antonio Rodrigues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
